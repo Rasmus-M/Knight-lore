8e82a32a7bb9ce4465573b20008d8d48d34cadf4
Rasmus-M <rasmus.moustgaard@gmail.com>
2017-07-06 17:05:53 +0200

    fixed move player bug

--- make.bat
+++ make.bat
@@ -1,1 +1,12 @@
+IF EXIST DOA.dsk GOTO :dsk_ok
+xdm99.py knightlore.dsk --initialize DSSD -n KNIGHTLORE
+:dsk_ok
+
 xas99.py -R -L knightlore.lst Source/knightlore.a99
+
+xas99.py -R -i Source/knightlore.a99 -o kl1
+
+xdm99.py knightlore.dsk -a kl1 -n KL1
+xdm99.py knightlore.dsk -a kl2 -n KL2
+xdm99.py knightlore.dsk -a kl3 -n KL3
+xdm99.py knightlore.dsk -a kl4 -n KL4
