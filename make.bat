03d87901d1e81cabe4f4ef019626f6ccd5e11f72
Rasmus-M <rasmus.moustgaard@gmail.com>
2017-07-06 18:25:39 +0200

    fixed move player bugs

--- make.bat
+++ make.bat
@@ -3,6 +3,7 @@ xdm99.py knightlore.dsk --initialize DSSD -n KNIGHTLORE
 :dsk_ok
 
 xas99.py -R -L knightlore.lst Source/knightlore.a99
+@IF %ERRORLEVEL% NEQ 0 GOTO :end
 
 xas99.py -R -i Source/knightlore.a99 -o kl1
 
@@ -10,3 +11,5 @@ xdm99.py knightlore.dsk -a kl1 -n KL1
 xdm99.py knightlore.dsk -a kl2 -n KL2
 xdm99.py knightlore.dsk -a kl3 -n KL3
 xdm99.py knightlore.dsk -a kl4 -n KL4
+
+:end
